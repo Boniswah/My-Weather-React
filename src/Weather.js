d2cca08c8cc79ac8dd0b03c36b57b38b78a34f39
Boniswah <boniswah@gmail.com>
2023-08-22 19:18:10 +0200

    Styling
    Added basic styling.

--- src/Weather.js
+++ src/Weather.js
@@ -5,34 +5,47 @@ export default function Weather() {
     return (
       <div className="weather">
         <form>
-            <div className="row">
-                <div className="col-9">
-                    <input type="search" placeholder="Enter a city..." className="form-control"/>
-                </div>
-                <div className="col-3">
-                  <input type="submit" value="Search" className="btn btn-primary"/>  
-                </div>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city..."
+                className="form-control"
+                autoFocus="on"
+              />
             </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
+            </div>
+          </div>
         </form>
         <h1>Kakamas</h1>
         <ul>
           <li>Friday 08:00</li>
           <li>Sunny</li>
         </ul>
-        <div className="row">
+        <div className="row mt-3">
           <div className="col-6">
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-              alt="Sunny"
-            />
-            29°C
+            <div className="d-flex">
+              <img
+                src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+                alt="Sunny"
+              />
+              <div className="float-left">
+                <span className="temperature">29</span>
+                <span className="unit">°C</span>
+              </div>
+            </div>
           </div>
           <div className="col-6">
             <ul>
-                <li>Precipitation: 0%</li>
-                <li>Humidity: 10%</li>
-                <li>Wind: 21km/h</li>
-
+              <li>Precipitation: 0%</li>
+              <li>Humidity: 10%</li>
+              <li>Wind: 21km/h</li>
             </ul>
           </div>
         </div>
