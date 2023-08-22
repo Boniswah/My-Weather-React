098677d23c9a840728aa42e6d6163e8ae8835cef
Boniswah <boniswah@gmail.com>
2023-08-22 09:46:10 +0200

    Update App.js
    added github and linkedin links

--- src/App.js
+++ src/App.js
@@ -4,7 +4,17 @@ import './App.css';
  export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://www.linkedin.com/in/boniswa-lebogo-58061346" target="_blank">
+          Boniswa G Lebogo
+        </a>{" "}
+        and is{" "}
+        <a href="https://github.com/Boniswah/My-Weather-React" target="_blank">
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
