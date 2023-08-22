b9ab754eeea66723c2491b19ad798548799236d2
Boniswah <boniswah@gmail.com>
2023-08-22 10:23:32 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,11 +7,19 @@ import './App.css';
       <h1>Weather App</h1>
       <footer>
         This project was coded by{" "}
-        <a href="https://www.linkedin.com/in/boniswa-lebogo-58061346" target="_blank">
+        <a
+          href="https://www.linkedin.com/in/boniswa-lebogo-58061346"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Boniswa G Lebogo
         </a>{" "}
         and is{" "}
-        <a href="https://github.com/Boniswah/My-Weather-React" target="_blank">
+        <a
+          href="https://github.com/Boniswah/My-Weather-React"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           open-sourced on GitHub
         </a>
       </footer>
